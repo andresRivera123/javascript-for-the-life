62ed3d594c877f1e8d1b4101c8b3284392225f53
andresRivera123 <88514682+andresRivera123@users.noreply.github.com>
2024-01-12 08:42:03 +1400

    Practica Register form 100%

--- 02 Conceptos clave js/6-Formularios/formulario/validateForm.js
+++ 02 Conceptos clave js/6-Formularios/formulario/validateForm.js
@@ -8,10 +8,40 @@ const userLastnameMessage = document.getElementById("userLastnameMessage");
 /* Country */
 const userCountry = document.getElementById("userCountry");
 const userCountryMessage = document.getElementById("userCountryMessage");
+/* Date */
+const userDate = document.getElementById("userDate");
+const userDateMessage = document.getElementById("userDateMessage");
+/* Email */
+const userEmail = document.getElementById("userEmail");
+const userEmailMessage = document.getElementById("userEmailMessage");
+/* Icon eye */
+const iconVisible = document.querySelectorAll(".icon__eye");
+/* Password */
+const userPassword = document.getElementById("userPassword");
+const userPasswordMessage = document.getElementById("userPasswordMessage");
+/* Repeat Password */
+const userPasswordRepeat = document.getElementById("userPasswordRepeat");
+const userPasswordRepeatMessage = document.getElementById(
+  "userPasswordRepeatMessage"
+);
+/* Template */
+const template = document.getElementById("template");
+const footer = document.querySelector("footer");
 
-const inputs = {};
+const user = {
+  userName: "",
+  userLastname: "",
+  userCountry: "",
+  userDate: "",
+  userEmail: "",
+  userPassword: "",
+};
+
 const validations = {
   regexpOnlyLetters: /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/, //Only letters
+  regexEmail: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+  regexPassword:
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, //password: 1 minuscule, 1 uppercase, 1 number, 1 special characters and have 8 characters minimum
 };
 
 const paintingErrors = (errors) => {
@@ -28,9 +58,46 @@ const paintingCorrect = (corrects) => {
     item.classList.remove("error__none");
     item.classList.remove("error");
     item.classList.add("correct");
-    item.textContent = "Correct 😃";
+    item.textContent = "✅";
   });
 };
+
+const changePropertyHidden = (e, number) => {
+  e.target.src = "./img/hidden.png";
+  e.target.id = "iconHidden" + number;
+  e.target.alt = "Hidden password";
+};
+
+const changePropertyVisible = (e, number) => {
+  e.target.src = "./img/visible.png";
+  e.target.id = "iconVisible" + number;
+  e.target.alt = "Visible password";
+};
+
+iconVisible.forEach((icon) => {
+  icon.addEventListener("click", (e) => {
+    /* button password */
+    if (e.target.id === "iconVisible1") {
+      changePropertyHidden(e, "1");
+      userPassword.type = "text";
+      return;
+    } else if (e.target.id === "iconHidden1") {
+      changePropertyVisible(e, "1");
+      userPassword.type = "password";
+      return;
+    }
+    /* button repeat password */
+    if (e.target.id === "iconVisible2") {
+      changePropertyHidden(e, "2");
+      userPasswordRepeat.type = "text";
+      return;
+    } else if (e.target.id === "iconHidden2") {
+      changePropertyVisible(e, "2");
+      userPasswordRepeat.type = "password";
+      return;
+    }
+  });
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -44,7 +111,7 @@ form.addEventListener("submit", (e) => {
       paragraphMessage: userNameMessage,
     });
   } else {
-    inputs[userName.id] = userName.value;
+    user.userName = userName.value;
     corrects.push(userNameMessage);
   }
 
@@ -56,26 +123,88 @@ form.addEventListener("submit", (e) => {
       paragraphMessage: userLastnameMessage,
     });
   } else {
-    inputs[userLastname.id] = userLastname.value;
+    user.userLastname = userLastname.value;
     corrects.push(userLastnameMessage);
   }
 
   /* Validate COUNTRY*/
-  if(userCountry.value === "nothing"){
+  if (userCountry.value === "nothing") {
     errors.push({
-        input: userCountry,
-        message: "Select an option",
-        paragraphMessage: userCountryMessage,
-    })
-  }else{
-    inputs[userCountry.id] = userCountry.value;
+      input: userCountry,
+      message: "Select an option",
+      paragraphMessage: userCountryMessage,
+    });
+  } else {
+    user.userCountry = userCountry.value;
     corrects.push(userCountryMessage);
   }
 
+  /* Validate DATE BIRTH */
+  if (userDate.value === "") {
+    errors.push({
+      input: userDate,
+      message: "Select your date",
+      paragraphMessage: userDateMessage,
+    });
+  } else {
+    user.userDate = userDate.value;
+    corrects.push(userDateMessage);
+  }
+
+  /* Validate EMAIL */
+  if (!validations.regexEmail.test(userEmail.value)) {
+    errors.push({
+      input: userEmail,
+      message: "Enter a valid email address",
+      paragraphMessage: userEmailMessage,
+    });
+  } else {
+    user.userEmail = userEmail.value;
+    corrects.push(userEmailMessage);
+  }
+
+  /* Validate PASSWORD */
+  if (!validations.regexPassword.test(userPassword.value)) {
+    errors.push({
+      input: userPassword,
+      message:
+        "8 characters minimum with uppercase, lowercase, numbers and special characters",
+      paragraphMessage: userPasswordMessage,
+    });
+  } else {
+    user.userPassword = userPassword.value;
+    corrects.push(userPasswordMessage);
+  }
+
+  /* Validate REPEAT PASSWORD */
+  if (userPasswordRepeat.value !== userPassword.value) {
+    errors.push({
+      input: userPasswordRepeat,
+      message: "Different passwords, retype it",
+      paragraphMessage: userPasswordRepeatMessage,
+    });
+  } else {
+    corrects.push(userPasswordRepeatMessage);
+  }
 
   paintingCorrect(corrects);
   paintingErrors(errors);
   if (errors.length === 0) {
-    console.log(inputs);
+    paintingTemplate();
   }
 });
+
+const paintingTemplate = () => {
+  const clone = template.content.cloneNode(true);
+  clone.querySelector("#completeName").textContent =
+    "Welcome " + user.userName + " " + user.userLastname;
+  clone.querySelector("#email").textContent = user.userEmail;
+  clone.querySelector("#country").textContent = user.userCountry;
+  clone.querySelector("#date").textContent = user.userDate
+  footer.appendChild(clone);
+};
+/* 
+    const clone = template.content.cloneNode();
+    clone.querySelector("#completeName").textContent = item.userName;
+    fragment.appendChild(clone); */
+/*   template.appendChild(fragment); */
