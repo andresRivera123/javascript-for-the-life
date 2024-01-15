ac624962abb461bed20044ee21a8f0852f5e6d5b
andresRivera123 <88514682+andresRivera123@users.noreply.github.com>
2024-01-15 23:13:22 +1400

    Proyecto paises 100% + filtar busqueda

--- 03 Conceptos avanzados JS/2-ajax-fetch/countrys/country.js
+++ 03 Conceptos avanzados JS/2-ajax-fetch/countrys/country.js
@@ -1,16 +1,66 @@
-document.addEventListener("DOMContentLoaded", () => {
-  getData();
+let dataCountry = [];
+document.addEventListener("DOMContentLoaded", async () => {
+  const data = await getData();
+  dataCountry = data.data;
+  paintTemplateCountry(dataCountry);
 });
 
 const getData = async () => {
   try {
-    const url = 'https://countriesnow.space/api/v0.1/countries/flag/images';
+    loading(true);
+    const url = "https://countriesnow.space/api/v0.1/countries/flag/images";
     const response = await fetch(url);
-    const data = await response.json()
-    console.log(data)
+    return await response.json();
   } catch (error) {
     console.log(error);
-  } finally{
-	console.log("SFASD")
+  } finally {
+    loading(false);
   }
 };
+
+const loadingSection = document.getElementById("loadingSection");
+const loading = (status) => {
+  if (status) {
+    loadingSection.classList.remove("d-none");
+  } else {
+    loadingSection.classList.add("d-none");
+  }
+};
+
+const paintTemplateCountry = (data) => {
+  const countryContainer = document.getElementById("countryContainer");
+  const templateCountry = document.getElementById("templateCountry").content;
+  const fragment = document.createDocumentFragment();
+  countryContainer.innerHTML = "";
+  data.forEach((item) => {
+    const clone = templateCountry.cloneNode(true);
+    clone.getElementById("isoCountry").textContent = item.iso3;
+    clone.getElementById("nameCountry").textContent = item.name;
+    clone.getElementById("imgCountry").setAttribute("src", item.flag);
+    clone.getElementById("imgCountry").alt = "Flag from " + item.name;
+
+    fragment.appendChild(clone);
+  });
+  countryContainer.appendChild(fragment);
+};
+
+const emptyCountryText = document.getElementById("emptyCountryText");
+const paintEmptyText = (empty) => {
+  if (empty === 0) {
+    emptyCountryText.classList.remove("d-none");
+  } else {
+    emptyCountryText.classList.add("d-none");
+  }
+};
+
+const inputSearchCountry = document.getElementById("inputSearchCountry");
+
+inputSearchCountry.addEventListener("keyup", (e) => {
+  const newCountrys = dataCountry.filter((item) =>
+    `${item.name.toLowerCase()}`.includes(
+      inputSearchCountry.value.toLowerCase()
+    )
+  );
+  paintEmptyText(newCountrys.length);
+  paintTemplateCountry(newCountrys);
+});
