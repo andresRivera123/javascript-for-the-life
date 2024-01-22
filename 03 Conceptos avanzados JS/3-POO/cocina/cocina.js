d36738bd6e58859d15dadaea864082013dc5e601
andresRivera123 <88514682+andresRivera123@users.noreply.github.com>
2024-01-22 23:16:37 +1400

    Practica cocina, eliminar cards por dataset

--- 03 Conceptos avanzados JS/3-POO/cocina/cocina.js
+++ 03 Conceptos avanzados JS/3-POO/cocina/cocina.js
@@ -10,7 +10,8 @@ const templateMesero = document.getElementById("templateMesero").content;
 const templateCocinero = document.getElementById("templateCocinero").content;
 
 class Persona {
-  constructor(nombre, cargo) {
+  constructor(id, nombre, cargo) {
+    this.id = id;
     this.nombre = nombre;
     this.cargo = cargo;
   }
@@ -18,16 +19,19 @@ class Persona {
   static pintarUI(personas, cargo) {
     const fragment = document.createDocumentFragment();
     if (cargo === "Cliente") {
+      clientesContenedor.textContent = "";
       personas.forEach((cliente) => {
         fragment.appendChild(cliente.crearNuevoCliente());
       });
       clientesContenedor.appendChild(fragment);
     } else if (cargo === "Mesero") {
+      meserosContenedor.textContent = "";
       personas.forEach((mesero) => {
         fragment.appendChild(mesero.crearNuevoMesero());
       });
       meserosContenedor.appendChild(fragment);
     } else if (cargo === "Cocinero") {
+      cocinerosContenedor.textContent = "";
       personas.forEach((cocinero) => {
         fragment.appendChild(cocinero.crearNuevoCocinero());
       });
@@ -37,8 +41,8 @@ class Persona {
 }
 
 class Cliente extends Persona {
-  constructor(nombre, cargo, pedido, totalPagar = 0) {
-    super(nombre, cargo);
+  constructor(id, nombre, cargo, pedido, totalPagar = 0) {
+    super(id, nombre, cargo);
     this.pedido = pedido;
     this.totalPagar = totalPagar;
   }
@@ -47,6 +51,7 @@ class Cliente extends Persona {
     const clone = templateCliente.cloneNode(true);
     clone.getElementById("nombreCliente").textContent = this.nombre;
     clone.getElementById("cargoCliente").textContent = this.cargo;
+    clone.getElementById("closeCard").dataset.idcard = this.id;
     return clone;
   }
 
@@ -66,6 +71,7 @@ class Mesero extends Persona {
     const clone = templateMesero.cloneNode(true);
     clone.getElementById("nombreMesero").textContent = this.nombre;
     clone.getElementById("cargoMesero").textContent = this.cargo;
+    clone.getElementById("closeCard").dataset.idcard = this.id;
     return clone;
   }
 
@@ -78,29 +84,56 @@ class Cocinero extends Persona {
     const clone = templateCocinero.cloneNode(true);
     clone.getElementById("nombreCocinero").textContent = this.nombre;
     clone.getElementById("cargoCocinero").textContent = this.cargo;
+    clone.getElementById("closeCard").dataset.idcard = this.id;
     return clone;
   }
 }
 
-const clientesArray = [];
-const meserosArray = [];
-const cocinerosArray = [];
+document.addEventListener("click", (e) => {
+  /* Eliminar cards por idcard */
+  if (e.target.matches("#closeCard")) {
+    const idcard = e.target.dataset.idcard;
+    if (e.target.dataset.idcard.includes("-cli")) {
+      clientesArray = clientesArray.filter(
+        (item) => item.id !== e.target.dataset.idcard
+      );
+      Persona.pintarUI(clientesArray, "Cliente");
+    }
+    if (e.target.dataset.idcard.includes("-mes")) {
+      meserosArray = meserosArray.filter(
+        (item) => item.id !== e.target.dataset.idcard
+      );
+      Persona.pintarUI(meserosArray, "Mesero");
+    }
+    if (e.target.dataset.idcard.includes("-coc")) {
+      cocinerosArray = cocinerosArray.filter(
+        (item) => item.id !== e.target.dataset.idcard
+      );
+      Persona.pintarUI(cocinerosArray, "Cocinero");
+      console.log(cocinerosArray);
+    }
+  }
+});
+
+let clientesArray = [];
+let meserosArray = [];
+let cocinerosArray = [];
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const persona = new FormData(form);
   const [nombre, cargo] = [...persona.values()];
+
   if (cargo === "Cliente") {
-    const cliente = new Cliente(nombre, cargo);
+    const cliente = new Cliente(Date.now() + "-cli", nombre, cargo);
     clientesArray.push(cliente);
     Persona.pintarUI(clientesArray, cargo);
   } else if (cargo === "Mesero") {
-    const mesero = new Mesero(nombre, cargo);
+    const mesero = new Mesero(Date.now() + "-mes", nombre, cargo);
     meserosArray.push(mesero);
     Persona.pintarUI(meserosArray, cargo);
   } else if (cargo === "Cocinero") {
-    const cocinero = new Cocinero(nombre, cargo);
+    const cocinero = new Cocinero(Date.now() + "-coc", nombre, cargo);
     cocinerosArray.push(cocinero);
     Persona.pintarUI(cocinerosArray, cargo);
-
   }
 });
